c9f48e64ad51977114bee474fc613e9fae9147a4
JieFangen <jiefangen@gmail.com>
2024-05-18 14:19:47 +0800

    离线IP区域查询集成

--- bamboo-core/src/main/java/org/panda/tech/core/web/util/IP2RegionUtil.java
+++ bamboo-core/src/main/java/org/panda/tech/core/web/util/IP2RegionUtil.java
@@ -6,6 +6,8 @@ import org.panda.bamboo.common.constant.basic.Strings;
 import org.panda.bamboo.common.util.LogUtil;
 import org.panda.tech.core.util.CommonUtil;
 
+import java.io.IOException;
+
 /**
  * IP转区域离线工具类
  *
@@ -14,6 +16,8 @@ import org.panda.tech.core.util.CommonUtil;
 public class IP2RegionUtil {
 
     private static final String IP_XDB_PATH = "META-INF/data/ip2region.xdb";
+
+    private volatile Searcher searcher;
 
     /**
      * 获取IP区域信息
@@ -36,5 +40,51 @@ public class IP2RegionUtil {
             LogUtil.error(IP2RegionUtil.class, "failed to search({}): {}\n", ip, e);
         }
         return null;
+    }
+
+    /**
+     * 加载缓存xdb数据的searcher对象
+     */
+    private void loadCachedSearcher() {
+        if (searcher == null) {
+            synchronized(this) { // 锁当前实例
+                if (searcher == null) {
+                    try {
+                        String dbPath = CommonUtil.getCurrentPath(IP2RegionUtil.class) + Strings.SLASH
+                                + Commons.PROJECT_RES_PATH + IP_XDB_PATH;
+                        byte[] cBuff = Searcher.loadContentFromFile(dbPath);
+                        searcher = Searcher.newWithBuffer(cBuff);
+                    } catch (Exception e) {
+                        LogUtil.error(IP2RegionUtil.class, "failed to load content cached searcher: {}\n", e);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取IP区域信息（xdb数据缓存）
+     *
+     * @param ip IP
+     * @return 区域信息
+     */
+    public String getIPRegionCache(String ip) {
+        loadCachedSearcher();
+        try {
+            return searcher.search(ip);
+        } catch (Exception e) {
+            LogUtil.error(IP2RegionUtil.class, "failed to search({}): {}\n", ip, e);
+        }
+        return null;
+    }
+
+    public void close() {
+        if (this.searcher != null) {
+            try {
+                this.searcher.close();
+            } catch (IOException e) {
+                // do nothing
+            }
+        }
     }
 }
